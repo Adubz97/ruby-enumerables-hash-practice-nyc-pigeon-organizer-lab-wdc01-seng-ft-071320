f584883e2d9b70108b72dcad1b9c11ea0eff40f9
Abdullah Albasha <abdullah.a.albasha@gmail.com>
2020-07-10 15:06:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list = {}
   data.each do |key, value|
-  value.each do |describe, arary|
+  value.each do |describe, array|
   array.each do |name|
     
     if pigeon_list.has_key?(name)

d7b89f17f7d882a24f40ab433aa67e7a86a6d507
Abdullah Albasha <abdullah.a.albasha@gmail.com>
2020-07-10 14:57:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,4 +7,10 @@
 
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |key, value|
+  value.each do |describe, arary|
+  array.each do |name|
+    
+    
 end
